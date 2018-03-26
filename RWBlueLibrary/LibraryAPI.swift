2ae0d2e6a9ac7ef37c992fac169d1b08db7ea6bc
LinuxPlus <>
2018-03-26 16:22:27 +0300

    new files

---
+++ RWBlueLibrary/LibraryAPI.swift
@@ -0,0 +1,18 @@
+//
+//    File.swift
+//    RWBlueLibrary
+//
+//    Created by LinuxPlus on 3/26/18.
+//    Copyright © 2018 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+final class LibraryAPI
+{
+	static let shared = LibraryAPI()
+	
+	private init() {
+		
+	}
+}
